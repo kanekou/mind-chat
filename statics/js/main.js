e99c479589ba3c105bd21284372c6f56e488c3f0
kanekou <e165726@ie.u-ryukyu.ac.jp>
2020-07-30 21:50:43 +0900

    add axios

--- statics/js/main.js
+++ statics/js/main.js
@@ -1,54 +1,79 @@
-(function(){
-	'use strict';
+Vue.config.devtools = true;
+(function () {
+  "use strict";
 
-	let vm = new Vue({
-		el: '#app',
-		data: {
-			formModal: false,
-			newMessage: '',
-			messages:[
-			]
-		},
-		created: function(){
-			// GET request
-			var self = this
-			  this.$http.get('http://db.denchu.cloud:5111/uiuxchat3287bivsgfbivf/test2/messages', function (data, status, request) {
-					for(var i = 0; i < data.length; i++){
-						this.messages.push(data[i]);
-					}
-				}).error(function (data, status, request) {
-					// handle error
-				})
-		},
-		methods:{
-			openFormModal: function(){
-				this.formModal = true
-			},
-			openSetting: function(index){
-				this.messages[index].setting = true
-			},
-			closeFormModal: function () {
-				this.formModal = false
-			},
-			closeSettingModal: function () {
-				this.messages[index].setting = false
-			},
-			addMessage: function(){
-				let item = {
-					text: this.newMessage,
-					good: 0,
-					setting: false
-				}
-				this.messages.push(item);
-				this.newMessage='';
-				this.formModal=false;
-			},
-			addGood: function(index){
-				this.messages[index].good++
-			},
-			deleteMessage: function(index){
-				this.messages.splice(index, 1);
-			}
-		}
-	});
+  let vm = new Vue({
+    el: "#app",
+    data: {
+      formModal: false,
+      newMessage: "",
+      messages: [],
+    },
+    mounted() {
+      axios
+        .get(
+          "http://db.denchu.cloud:5111/uiuxchat3287bivsgfbivf/test2/messages"
+        )
+        .then(
+          (response) => {
+            for (let i = 0; i < response.data.messages.length; i++) {
+              this.messages.push(response.data.messages[i].content);
+            }
+            console.log(messages[0]);
+            // this.messages.push(response.data.messages)
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+    },
+    methods: {
+      //   fetchTasks: function () {
+      //     axios
+      //       .get(
+      //         "http://db.denchu.cloud:5111/uiuxchat3287bivsgfbivf/test2/messages"
+      //       )
+      //       .then(
+      //         (response) => {
+      //           for (let i = 0; i < response.data.messages.length; i++) {
+      //             this.messages.push(response.data.messages[i]);
+      //           }
+      //           console(messages[0]);
+      //           // this.messages.push(response.data.messages)
+      //         },
+      //         (error) => {
+      //           console.log(error);
+      //         }
+      //       );
+      //   },
+      openFormModal: function () {
+        this.formModal = true;
+      },
+      openSetting: function (index) {
+        this.messages[index].setting = true;
+      },
+      closeFormModal: function () {
+        this.formModal = false;
+      },
+      closeSettingModal: function () {
+        this.messages[index].setting = false;
+      },
+      addMessage: function () {
+        let item = {
+          text: this.newMessage,
+          good: 0,
+          setting: false,
+        };
+        this.messages.push(item);
+        this.newMessage = "";
+        this.formModal = false;
+      },
+      addGood: function (index) {
+        this.messages[index].good++;
+      },
+      deleteMessage: function (index) {
+        this.messages.splice(index, 1);
+      },
+    },
+  });
 })();
